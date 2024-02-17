384affbbb7c9e68ad9eef93735d31e59dcc5a427
rahgirrafi <rahgirrafi@gmail.com>
2024-02-18 04:16:58 +0600

    Implementing Classes

---
+++ src/main/java/com/example/planner/CalendarEventManager.java
@@ -0,0 +1,43 @@
+package com.example.planner;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.time.LocalDate;
+
+public class CalendarEventManager {
+
+	private final ObservableList<CalendarEvent> eventList;
+
+	public CalendarEventManager() {
+		eventList = FXCollections.observableArrayList();
+	}
+
+	public void addEvent(CalendarEvent event) {
+		eventList.add(event);
+	}
+
+	public void removeEvent(CalendarEvent event) {
+		for (CalendarEvent e : eventList) {
+			if (e.getId() == event.getId()) {
+				eventList.remove(e);
+				break;
+			}
+		}
+
+	}
+
+	public ObservableList<CalendarEvent> getEventsOn(LocalDate localDate) {
+
+		ObservableList<CalendarEvent> currentEventList = FXCollections
+				.observableArrayList();
+
+		for (CalendarEvent event : eventList) {
+			if (event.isOnDate(localDate)) {
+				currentEventList.add(event);
+			}
+		}
+
+		return currentEventList;
+	}
+}
